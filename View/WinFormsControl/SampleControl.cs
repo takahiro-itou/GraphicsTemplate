0d8dfc537f25c6170839146041b05e88a6d710d0
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-09-03 20:13:53 +0900

    プロパティとメソッドを実装

--- View/WinFormsControl/SampleControl.cs
+++ View/WinFormsControl/SampleControl.cs
@@ -3,81 +3,71 @@ using System.ComponentModel;
 
 namespace WinFormsControl
 {
-    public partial class SampleControl : UserControl
+
+public partial class SampleControl : UserControl
+{
+    public SampleControl()
     {
-        public SampleControl()
-        {
-            InitializeComponent();
-        }
-
-#if false
-        //----------------------------------------------------------------
-        //    InputText プロパティ
-        //
-        [Browsable(true)
-          , Description("入力テキストボックスに入力された文字列")
-          , Category("表示")
-        ]
-        public String InputText
-        {
-            get { return txtInput.Text; }
-            set { txtInput.Text = value; }
-        }
-
-        //----------------------------------------------------------------
-        //    OutputText プロパティ
-        //
-        [Browsable(true)
-          , Description("出力テキストボックスに表示する文字列")
-          , Category("表示")
-        ]
-        public String OutputText
-        {
-            get { return txtOutput.Text; }
-            set { txtOutput.Text = value; }
-        }
-#endif
-
-        //----------------------------------------------------------------
-        /**   RunButtonClick イベント
-        **
-        **/
-        [Browsable(true)
-          , Description("実行ボタンがクリックされた時に発生します")
-          , Category("アクション")
-        ]
-        public event EventHandler<EventArgs>? RunButtonClick;
-
-        //----------------------------------------------------------------
-        /**   イベントハンドラを呼び出すメソッド
-        **
-        **/
-        protected virtual void OnRunButtonClick(EventArgs e)
-        {
-            var eventHandler = RunButtonClick;
-            if (eventHandler != null) {
-                eventHandler(this, e);
-            }
-        }
-
-        //----------------------------------------------------------------
-        /**   イベントハンドラ
-        **
-        **    「クリア」ボタンのクリックイベント
-        **/
-        private void btnClear_Click(object sender, EventArgs e)
-        {
-        }
-
-        //----------------------------------------------------------------
-        /**   イベントハンドラ
-        **
-        **    「実行」ボタンのクリックイベント
-        **/
-        private void btnRun_Click(object sender, EventArgs e)
-        {
-            OnRunButtonClick(e);
-        }
-
+        InitializeComponent();
     }
-}
+
+    //----------------------------------------------------------------
+    /**   MarginAreaColor プロパティ
+    **
+    **/
+    [Browsable(true)
+      , Description("余白部分の背景色")
+      , Category("表示")
+    ]
+    public System.Drawing.Color MarginAreaColor
+    {
+        get { return  this.m_marginColor; }
+        set { this.m_marginColor = value; }
+    }
+
+    //----------------------------------------------------------------
+    /**   RunButtonClick イベント
+    **
+    **/
+    [Browsable(true)
+      , Description("実行ボタンがクリックされた時に発生します")
+      , Category("アクション")
+    ]
+    public event EventHandler<EventArgs>? RunButtonClick;
+
+    //----------------------------------------------------------------
+    /**   イベントハンドラを呼び出すメソッド
+    **
+    **/
+    protected virtual void OnRunButtonClick(EventArgs e)
+    {
+        var eventHandler = RunButtonClick;
+        if (eventHandler != null) {
+            eventHandler(this, e);
+        }
+    }
+
+    //----------------------------------------------------------------
+    /**   イベントハンドラ
+    **
+    **    「クリア」ボタンのクリックイベント
+    **/
+    private void btnClear_Click(object sender, EventArgs e)
+    {
+    }
+
+    //----------------------------------------------------------------
+    /**   イベントハンドラ
+    **
+    **    「実行」ボタンのクリックイベント
+    **/
+    private void btnRun_Click(object sender, EventArgs e)
+    {
+        OnRunButtonClick(e);
+    }
+
+    private System.Drawing.Color    m_marginColor;
+
+}   //  End of class SampleControl
+
+}   //  End of namespace WinFormsControl
