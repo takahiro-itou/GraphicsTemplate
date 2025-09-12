97ec2784fb327b34a2b036d591eaeeecddc2b5dc
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-09-12 19:47:36 +0900

    空のクラスを新規作成

---
+++ View/Core/Sample/Images/BitmapRenderer.cpp
@@ -0,0 +1,104 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---   Graphics Test Project.   ---                  **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of BitmapRenderer class.
+**
+**      @file       Images/BitmapRenderer.cpp
+**/
+
+#include    "Sample/Images/BitmapRenderer.h"
+
+
+SAMPLE_NAMESPACE_BEGIN
+namespace  Images  {
+
+namespace  {
+
+}   //  End of (Unnamed) namespace.
+
+
+//========================================================================
+//
+//    BitmapRenderer  class.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//----------------------------------------------------------------
+//    インスタンスを初期化する
+//  （デフォルトコンストラクタ）。
+
+BitmapRenderer::BitmapRenderer()
+{
+}
+
+//----------------------------------------------------------------
+//    インスタンスを破棄する
+//  （デストラクタ）。
+//
+
+BitmapRenderer::~BitmapRenderer()
+{
+}
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//----------------------------------------------------------------
+//    サンプル画像を描画する。
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+}   //  End of namespace  Images
+SAMPLE_NAMESPACE_END
---
+++ View/Core/Sample/Images/BitmapRenderer.h
@@ -0,0 +1,126 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---   Graphics Test Project.   ---                  **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of BitmapRenderer class.
+**
+**      @file       Images/BitmapRenderer.h
+**/
+
+#pragma once
+
+#if !defined( SAMPLE_IMAGES_INCLUDED_BITMAP_RENDERER_H )
+#    define   SAMPLE_IMAGES_INCLUDED_BITMAP_RENDERER_H
+
+#include    "Sample/Common/SampleSettings.h"
+
+
+SAMPLE_NAMESPACE_BEGIN
+namespace  Images  {
+
+//========================================================================
+//
+//    BitmapRenderer  class.
+//
+
+class  BitmapRenderer
+{
+
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   インスタンスを初期化する
+    **  （デフォルトコンストラクタ）。
+    **
+    **/
+    BitmapRenderer();
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する
+    **  （デストラクタ）。
+    **
+    **/
+    virtual  ~BitmapRenderer();
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+//========================================================================
+//
+//    Other Features.
+//
+private:
+    typedef     BitmapRenderer      This;
+    BitmapRenderer      (const  This  &);
+    This &  operator =  (const  This  &);
+public:
+    //  テストクラス。  //
+    friend  class   BitmapRendererTest;
+};
+
+}   //  End of namespace  Images
+SAMPLE_NAMESPACE_END
+
+#endif
