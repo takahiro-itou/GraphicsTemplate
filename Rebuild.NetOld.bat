cdb75a8d0e8512fe11f18be0a5db94f61ac7b9bc
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-08-11 15:54:19 +0900

    ビルド用バッチファイルを修正

--- Rebuild.NetOld.bat
+++ Rebuild.NetOld.bat
@@ -1,4 +1,12 @@
 
-msbuild -restore -t:Rebuild     ^
-    -p:Configuration="Release"  -p:Platform=x64     ^
-    SampleView.NetOld.sln
+set  solution=SampleView
+set  config="Debug"
+
+
+msbuild  -restore  -t:Clean     ^
+    -p:Configuration=%config%   -p:Platform=x64     ^
+    "%solution%.NetOld.sln"
+
+msbuild  -restore  -t:Rebuild   ^
+    -p:Configuration=%config%   -p:Platform=x64     ^
+    "%solution%.NetOld.sln"
